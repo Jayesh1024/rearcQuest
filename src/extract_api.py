ef1b1834f2e0853c577b13504a0eaf07f8f8290b
jayesh.mhaske <jayesh.mhaske@searce.com>
2026-05-10 16:58:23 +0530

    1. Add requirements.txt 2. Add S3 integration for writing to S3

--- src/extract_api.py
+++ src/extract_api.py
@@ -1,7 +1,17 @@
 import requests
 import json
 import os
-import datetime
+import boto3, dotenv
+
+dotenv.load_dotenv(override=True)
+
+s3 = boto3.client('s3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
+)
+
+bucket = os.getenv('AWS_BUCKET')
 
 url = 'https://honolulu-api.datausa.io/tesseract/data.jsonrecords?cube=acs_yg_total_population_1&drilldowns=Year%2CNation&locale=en&measures=Population&limit=1000&offset=0'
 
@@ -13,5 +23,5 @@ def extract_api(url:str) -> None:
         raise Exception(f"Failed to get data from {url}, with status code {response.status_code}")
 
 data = extract_api(url)
-with open('output/api.json', 'w') as f:
-    json.dump(data, f, indent=4)
+
+s3.put_object(Bucket=bucket, Key='api/data.json', Body=json.dumps(data, indent=4).encode('utf-8'))
--- src/extract_csv.py
+++ src/extract_csv.py
@@ -4,6 +4,8 @@ import re
 import json
 import os
 
+
+
 headers = {
     "Accept-Language": "en-US,en;q=0.9",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
@@ -32,8 +34,8 @@ def sync_files(files:list[tuple[str, str]], metadata_fp:str) -> None:
     for url, date in files:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            with open(f'output/{url.split("/")[-1]}.csv', 'w') as f:
-                f.write(response.text)
+            key = f'csv/{url.split("/")[-1]}.csv'
+            s3.put_object(Bucket=bucket, Key=key, Body=response.content)
         else:
             raise Exception(f"Failed to get file from {url}, with status code {response.status_code}")
 
@@ -44,24 +46,25 @@ def sync_files(files:list[tuple[str, str]], metadata_fp:str) -> None:
             'ingestion_time': datetime.now().isoformat()
         })
         incoming.add(url.split('/')[-1])
-
+    
     # --- Handle deletes ---
-    with open(metadata_fp, 'r') as f:
-        metadata_current = json.load(f)
+    try:
+        metadata_current = s3.get_object(Bucket=bucket, Key=metadata_fp)
+        metadata_current = json.loads(metadata_current['Body'].read().decode('utf-8'))
+        current:set[str] = set()
+        for file in metadata_current:
+            current.add(file['filename'])
 
-    current:set[str] = set()
-    for file in metadata_current:
-        current.add(file['filename'])
+        to_delete = current - incoming
+        for file in metadata_current:
+            if file['filename'] in to_delete:
+                s3.delete_object(Bucket=bucket, Key=f'csv/{file["filename"]}.csv')
+    except Exception as e:
+        print(f"No metadata file found, this may be the first time running the script, error: {e}")
 
-    to_delete = current - incoming
-    for file in metadata_current:
-        if file['filename'] in to_delete:
-            os.remove(f'output/{file["filename"]}.csv')
-
-    with open(metadata_fp, 'w') as f:
-        json.dump(metadata_incoming, f, indent=4)
+    s3.put_object(Bucket=bucket, Key=metadata_fp, Body=json.dumps(metadata_incoming, indent=4).encode('utf-8'))
 
 
 full_url = base + path
 files = list_files(full_url, headers)
-sync_files(files, 'output/metadata.json')
+sync_files(files, 'config/metadata.json')
