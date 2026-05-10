5c2f8766e84d1b1026615689ff1d77ddf373d532
jayesh.mhaske <jayesh.mhaske@searce.com>
2026-05-10 16:59:33 +0530

    Fix mistake

--- src/extract_csv.py
+++ src/extract_csv.py
@@ -3,7 +3,17 @@ import requests
 import re
 import json
 import os
+import boto3, dotenv
 
+dotenv.load_dotenv(override=True)
+
+s3 = boto3.client('s3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
+)
+
+bucket = os.getenv('AWS_BUCKET')
 
 
 headers = {
