d50cdcd8142739670c8fde57342c71e09681b48a
jayesh.mhaske <jayesh.mhaske@searce.com>
2026-05-11 21:50:18 +0530

    Modfir main.py so api call happens after the csv data ingestion

--- src/main.py
+++ src/main.py
@@ -8,14 +8,14 @@ s3 = boto3.client('s3',
 
 bucket = os.getenv('AWS_BUCKET')
 def handler(event:dict, context:dict) -> None:
-    # --- Extract the API data ---
-    data = extract_api(url_api)
-    s3.put_object(Bucket=bucket, Key='api/data.json', Body=json.dumps(data, indent=4).encode('utf-8'))
-
     # --- Extract the CSV data ---
     full_url = base + path
     files = list_files(full_url, headers)
     sync_files(files, 'config/metadata.json', s3, bucket)
+
+    # --- Extract the API data ---
+    data = extract_api(url_api)
+    s3.put_object(Bucket=bucket, Key='api/data.json', Body=json.dumps(data, indent=4).encode('utf-8'))
 
     return {
         'statusCode': 200
